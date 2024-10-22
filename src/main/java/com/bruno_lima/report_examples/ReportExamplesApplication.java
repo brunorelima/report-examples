a479ebb2841678223dff5a41899d6aa5214b603d
Bruno Lima <bruno.lima@nuacom.ie>
2024-10-22 17:23:46 -0300

    feat: create the project

---
+++ src/main/java/com/bruno_lima/report_examples/ReportExamplesApplication.java
@@ -0,0 +1,13 @@
+package com.bruno_lima.report_examples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReportExamplesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bruno_lima/report_examples/ReportExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bruno_lima.report_examples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportExamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
